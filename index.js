927de6a4c84a259bc750d1022c0c7c25f807385d
HKiwooma <hkiwooma@gmail.com>
2020-02-10 13:14:46 +0300

    modified index.js

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const Laptop = require('./models/laptops');
 const dell = new Laptop(
   '320GB',
   4,
+  '2.4Ghz',
   '4hrs',
   '15px',
   '7 Generation',
   'Dell',
-  '$1200',
+  1200,
   'debian 10',
   80293845480,
+  1989,
 );
 console.log(dell.printableString());
-console.log(dell.ram);
 
